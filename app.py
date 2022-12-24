0cc18076bee11aaa5f9570627902d1ddbd15846e
Stasensus <stanislav@kireev.ru>
2022-12-24 11:17:56 +0300

    Работаем над проектом

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/auth')
+def auth():
+    return render_template('auth.html')
 
-def index():
-    return render_template('index.html')
+@app.route('/account', methods = ['GET', 'POST'])
+def account():
+    if request.method == 'GET':
+        return render_template('account.html')
+    project = request.form.get('project')
+    login = request.form.get('login')
+    password = request.form.get('password')
+    print(project, login, password)
+
+    return render_template('account.html')
+
+@app.route('/end')
+def end():
+    return render_template('end.html')
 
 if __name__ == '__main__':
     app.run()
